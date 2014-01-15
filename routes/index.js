a30ff0c66de5b0b71579f1c56b712fb1df47bd8c
zhuqiang <zhuqiang@WIN-J8EVTUEQTQ4>
2014-01-15 17:33:13 +0800

    路由控制

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+//exports.index = function(req, res){
+//  res.render('index', { title: 'Express' });
+//};
+
+module.exports = function(app1){
+	app.get('/',function(req,res){
+		res.render('index',{title:'Express模块'})
+	});
+};
+
+
+module.exports = function(app2){
+	app.get('/',function(req,res){
+		res.render('index',{title:'Express模块'})
+	});
 };
